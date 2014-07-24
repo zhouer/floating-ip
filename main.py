1ed2262ab2842872b36ae8ccd25947670004f549
En-Ran Zhou <zhouer@gmail.com>
2014-07-24 15:47:16 -0500

    Implement basic feature.

---
+++ main.py
@@ -0,0 +1,35 @@
+import webapp2
+from google.appengine.ext import ndb
+
+class Log(ndb.Model):
+    access = ndb.DateTimeProperty(auto_now_add=True)
+    path = ndb.StringProperty()
+    ip = ndb.StringProperty()
+
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        self.response.headers['Content-Type'] = 'text/plain'
+
+        for p in Log.query(projection=["path"], distinct=True):
+            rs = Log.query(Log.path == p.path).order(-Log.access).fetch(1)
+            if rs:
+                r = rs[0]
+                self.response.write(r.path + "\n\tIP: " +  r.ip + "\n\tAccess: " + r.access.isoformat() + "\n\n")
+
+class LogPage(webapp2.RequestHandler):
+    def get(self, path):
+        if path == 'favicon.ico':
+            return
+
+        log = Log()
+        log.path = path
+        log.ip = self.request.remote_addr
+        log.put()
+
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.write('Access %s from %s' % (path, self.request.remote_addr))
+
+application = webapp2.WSGIApplication([
+    ('/', MainPage),
+    ('/(.*)', LogPage),
+], debug=True)
